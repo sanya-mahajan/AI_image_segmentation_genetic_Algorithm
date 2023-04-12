0438b281733dca7f600a1f77c416e2849a0f7f4e
sanya-mahajan <sanyamahajan08@gmail.com>
2023-04-12 14:14:24 +0530

    upt

--- otsu.py
+++ otsu.py
@@ -15,7 +15,7 @@ def fitness_func(threshold, hist):
     return variance
 
 def genetic_algorithm(hist, population_size=100, num_generations=20, mutation_rate=0.05):
-    # Initialize the population
+    # initial  population
     population = np.random.randint(0, 256, (population_size,))
 
     # Loop through generations
@@ -23,18 +23,20 @@ def genetic_algorithm(hist, population_size=100, num_generations=20, mutation_ra
 
         fitness_scores = np.array([fitness_func(threshold, hist) for threshold in population])
 
-        # Select the fittest 
+        # select the fittest 
         fittest_indices = np.argsort(fitness_scores)[-int(population_size * 0.2):]
         fittest_individuals = population[fittest_indices]
 
-        # Generate offspring
+        # Generate offsp
+
         offspring = []
         for j in range(population_size):
             parent1 = fittest_individuals[np.random.randint(len(fittest_individuals))]
             parent2 = fittest_individuals[np.random.randint(len(fittest_individuals))]
             offspring.append((parent1 + parent2) // 2)
 
-        # Mutatation
+        # mutation acc to rate
+        
         for j in range(len(offspring)):
             if np.random.rand() < mutation_rate:
                 offspring[j] = np.random.randint(0, 256)
@@ -58,13 +60,16 @@ hist, _ = np.histogram(gray, bins=256)
 threshold = genetic_algorithm(hist)
 
 # Threshold the image and display the result
-# binary = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)[1]
-# cv2.imshow('binary image', binary)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+binary = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)[1]
+cv2.imshow('binary image', binary)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# Display the histogram with the optimal threshold
-fig, ax = plt.subplots()
-ax.hist(gray.ravel(), bins=256)
-ax.axvline(threshold, color='red', linestyle='dashed', linewidth=2)
-plt.show()
+# display histogram with threshold
+# fig, ax = plt.subplots()
+# ax.hist(gray.ravel(), bins=256)
+# ax.axvline(threshold, color='red', linestyle='dashed', linewidth=2)
+# plt.show()
+
+
+#can take user input for crossover and mutation rate 
