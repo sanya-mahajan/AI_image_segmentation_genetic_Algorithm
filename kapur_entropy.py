1ffb31ea5decaf915b0f1bc78159343e3534a4fe
sanya-mahajan <sanyamahajan08@gmail.com>
2023-04-18 14:48:33 +0530

    path

--- kapur_entropy.py
+++ kapur_entropy.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 img = cv2.imread('images/nuc.png', 0)
-cv2.namedWindow('Input image', cv2.WINDOW_NORMAL)
-cv2.imshow('Input image', img)
+
+orig=img
 if img is None:
     print("Error: could not load image")
     exit()
@@ -16,11 +16,11 @@ hist, bins = np.histogram(img.flatten(), 256, [0, 256])
 cdf = hist.cumsum()
 cdf_normalized = cdf / float(cdf.max())
 
-# Kapur entropy function for all possible threshold values
+# kapur entropy function for all possible threshold values
 thresholds = range(256)
 entropies = []
 for t in thresholds:
-    # Calculate the probability distribution function (PDF) for each class
+    #  probability distribution function (PDF) for each class
     pdf1 = cdf_normalized[t]
     pdf2 = 1 - pdf1
     if pdf1 == 0 or pdf2 == 0:
@@ -36,16 +36,16 @@ for t in thresholds:
 # find  threshold value that maximizes the Kapur entropy 
 optimal_threshold = thresholds[np.argmax(entropies)]
 
-#  threshold the image
+
 img_seg = np.zeros_like(img)
 img_seg[img > optimal_threshold] = 255
 cv2.namedWindow('seg image', cv2.WINDOW_NORMAL)
-
+cv2.namedWindow('Input image', cv2.WINDOW_NORMAL)
 cv2.resizeWindow("seg image", 300, 700)
 cv2.resizeWindow("Input image", 300, 700)
 
 cv2.imshow('seg image', img_seg)
-
+cv2.imshow('Input image', orig)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
